cc86523b356f736512d3eeaa59b263526d742536
fontcuberta <isafontcu@gmail.com>
2016-05-21 20:36:42 +0200

    DDBB Populated

--- AngelHackBcn/db/seeds.rb
+++ AngelHackBcn/db/seeds.rb
@@ -96,3 +96,20 @@ topic.description = "Finance"
 topic.save
 
 #Initiative
+
+initiative = Initiative.new
+initiative.title = "Raise the minimum wage"
+initiative.content = "If you work hard you should be compensated fairly. Rise minimum wage to $15 per hour"
+initiative.save
+
+initiative = Initiative.new
+initiative.title = "Renewable energy"
+initiative.content = "Invest in cleaner renewable energy and fund research and development"
+initiative.save
+
+initiative = Initiative.new
+initiative.title = "Taxes initiatives"
+initiative.content = "Raise taxes on high-income taxpayers, modify taxation of multinational corporations, repeal fossil fuel tax incentives, and increasing estate and gift taxes"
+initiative.save
+
+
