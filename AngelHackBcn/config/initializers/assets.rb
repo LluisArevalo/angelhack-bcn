35c02f4aea466242e5350fb672445130b05e1210
LluisArevalo <thorin119@gmail.com>
2016-05-21 16:39:20 +0200

    Add main photo

--- AngelHackBcn/config/initializers/assets.rb
+++ AngelHackBcn/config/initializers/assets.rb
@@ -11,3 +11,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( reset.css )
 Rails.application.config.assets.precompile += %w( public.css )
+Rails.application.config.assets.precompile << /\.(?:png|jpg|jpeg|gif)\z/
