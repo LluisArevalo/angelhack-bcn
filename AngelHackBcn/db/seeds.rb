5ce04c0c85b5f9bfd249267429eb0a8884c7e5fc
fontcuberta <isafontcu@gmail.com>
2016-05-21 18:10:43 +0200

    Seeding users

--- AngelHackBcn/db/seeds.rb
+++ AngelHackBcn/db/seeds.rb
@@ -6,3 +6,32 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+user = User.new
+user.email = "paco@perez.com"
+user.password = '1234567890'
+user.save
+
+user = User.new
+user.email = "josefina@lopez.com"
+user.password = '1234567890'
+user.save
+
+user = User.new
+user.email = "sara@garcia.com"
+user.password = '1234567890'
+user.save
+
+user = User.new
+user.email = "marta@fonda.com"
+user.password = '1234567890'
+user.save
+
+user = User.new
+user.email = "lluis@albert.com"
+user.password = '1234567890'
+user.save
+
+user = User.new
+user.email = "raul@vega.com"
+user.password = '1234567890'
+user.save
