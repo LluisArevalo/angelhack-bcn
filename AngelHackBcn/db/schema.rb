467c83c829c7a02fb4dca657990c31fbc0654a24
LluisArevalo <thorin119@gmail.com>
2016-05-21 20:36:31 +0200

    Main page, first and second sections

--- AngelHackBcn/db/schema.rb
+++ AngelHackBcn/db/schema.rb
@@ -36,8 +36,6 @@ ActiveRecord::Schema.define(version: 20160521143115) do
   add_index "action_statuses", ["user_id"], name: "index_action_statuses_on_user_id"
 
   create_table "actions", force: :cascade do |t|
-    t.string   "title"
-    t.text     "content"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.string   "image_file_name"
@@ -94,4 +92,5 @@ ActiveRecord::Schema.define(version: 20160521143115) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
 end
