61350855f65e6e207098dd9ad0807a84a48abc86
Raul Vega <Raulvv@Rauls-MacBook-Air.local>
2016-05-21 23:00:38 +0200

    changed relations between topic/region and initiatives

--- AngelHackBcn/app/models/region.rb
+++ AngelHackBcn/app/models/region.rb
@@ -1,3 +1,3 @@
 class Region < ActiveRecord::Base
-  belongs_to :initiative
+  has_many :initiatives
 end
--- AngelHackBcn/app/models/topic.rb
+++ AngelHackBcn/app/models/topic.rb
@@ -1,3 +1,3 @@
 class Topic < ActiveRecord::Base
-  belongs_to :initiative
+  has_many :initiatives
 end
