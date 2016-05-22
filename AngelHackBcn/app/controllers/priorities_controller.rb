7fe61884bd5344801fa5c1fd67222049d2e87467
Raul Vega <Raulvv@Rauls-MacBook-Air.local>
2016-05-22 10:31:51 +0200

    Vote and set initiative priority, back funcionality

--- AngelHackBcn/app/controllers/priorities_controller.rb
+++ AngelHackBcn/app/controllers/priorities_controller.rb
@@ -1,8 +1,14 @@
 class PrioritiesController < ApplicationController
-  
-
-  private
-  def initiative_params
-    params.require(:initiative).permit(:title, :content, :topic_id, :region_id)
+  def create
+    priority_assignation = PriorityAssignation.new(
+                            initiative_id: params[:initiative_id],
+                            priority_id: params[:priority_id],
+                            user_id: current_user.id
+                          )
+    if priority_assignation.save
+      priority = PriorityAssignation.set_priority params[:initiative_id]
+      initiative = Initiative.find params[:initiative_id]
+      initiative.update_attribute(priority_id: priority)
+    end
   end
 end
