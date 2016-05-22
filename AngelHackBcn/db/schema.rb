225b8c03f5214bc7c64485260f60f54c0c7cc802
Marta Fonda <martafondapascual@gmail.com>
2016-05-22 12:14:51 +0200

    Main structure and Sass styling for initiatives index and footer

--- AngelHackBcn/db/schema.rb
+++ AngelHackBcn/db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160522081156) do
-
 
   create_table "initiative_comments", force: :cascade do |t|
     t.integer  "user_id"
